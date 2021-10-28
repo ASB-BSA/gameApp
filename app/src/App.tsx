533ed929d4f8b20b4f7931187064a904280d67f7
tagawa shouta <sho.webfe@gmail.com>
2021-10-28 19:17:35 +0900

    destyle

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,27 +1,9 @@
 import React from 'react';
-import logo from '@imgs/logo.svg';
-import '@css/global/App.css';
-import styles from '@css/module/sample.module.scss';
-import { TestComponent } from '@/components';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className={styles.sampleComponent}>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <TestComponent />
+      こんにちは
     </div>
   );
 }
