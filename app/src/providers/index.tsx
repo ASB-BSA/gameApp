493e76c95f8ad091e919e014f920cac1f5efc800
tagawa shouta <sho.webfe@gmail.com>
2021-11-03 16:11:39 +0900

    recoilの設定

--- app/src/providers/index.tsx
+++ app/src/providers/index.tsx
@@ -1,5 +1,13 @@
+import {
+  RecoilRoot,
+} from 'recoil';
+
 const Provider: React.FC = ({ children }) => {
-  return <>{children}</>
+  return (
+    <RecoilRoot>
+      {children}
+    </RecoilRoot>
+  )
 }
 
 export default Provider;
---
+++ app/src/types/UserType.ts
@@ -0,0 +1,13 @@
+export type UserType = {
+  isSignIn: boolean,
+  id: number,
+  name: string,
+}
+
+export type UserJsonType = {
+  ID: number,
+  CreatedAt: string,
+  UpdatedAt: string,
+  DeletedAt?: string,
+  name: string,
+}
