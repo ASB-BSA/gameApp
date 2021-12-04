f1db180cd34dad96f2a91464ec20a9c0c1688d1f
sakuyasann <sakuya@sakuyasan.net>
2021-12-04 13:46:05 +0900

    へるすばー

--- app/src/pages/Auth/Battle/Battle.tsx
+++ app/src/pages/Auth/Battle/Battle.tsx
@@ -3,14 +3,13 @@ import React from 'react'
 import BackGround from '@/assets/imgs/team/team__bg.jpg'
 import FieldBg from '@/assets/imgs/battle/battle__view.jpg'
 import styled from 'styled-components'
+import { HealthBar } from '@/components/pages/auth/battle'
 
 const Battle = () => {
   return (
     <Wrapper bg={BackGround}>
       <MainView>
-        <HealthBarBox>
-
-        </HealthBarBox>
+        <HealthBar />
         batoru
       </MainView>
     </Wrapper>
@@ -22,9 +21,4 @@ export default Battle
 const MainView = styled.div`
   min-height: 980px;
   background: url(${FieldBg}) no-repeat center center / cover;
-`
-
-const HealthBarBox = styled.div`
-  background: #8e6441;
-  border-bottom: 1px solid #4d2b0e;
 `
