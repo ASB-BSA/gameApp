dc71f54bfda23ecf568eae8a199c94996eab0681
tagawa shouta <sho.webfe@gmail.com>
2021-12-11 15:30:03 +0900

    ログイン時に全キャラクターデータを取得する

--- app/src/hooks/useAnonymousUser.ts
+++ app/src/hooks/useAnonymousUser.ts
@@ -1,10 +1,12 @@
+import type { UserJsonType } from '@/types/UserType';
 import { useState } from 'react';
 import { useUserRecoil } from '@/recolis';
-import { UserJsonType } from '@/types/UserType';
+import { useCharacterData } from '@/recolis/charcterData/handler';
 import axios from 'axios';
 
 const useAnonymousUser = () => {
   const { setUser } = useUserRecoil();
+  const { fetchCharacterData } = useCharacterData();
   const [name, setName] = useState<string>('');
   const [wait, setWait] = useState<boolean>(false);
   const [redirect, setRedirect] = useState<boolean>(false);
@@ -24,6 +26,8 @@ const useAnonymousUser = () => {
         name: res.data.name,
       })
 
+      await fetchCharacterData();
+      
       setWait(false);
       setName('');
       setRedirect(true);
