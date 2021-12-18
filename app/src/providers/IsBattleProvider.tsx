d6b5a00450b5b1278ab61e625cb39fad0bac686c
sakuyasann <sakuya@sakuyasan.net>
2021-12-18 12:28:38 +0900

    バトル情報のアクティブかどうかの判断を追加

--- app/src/providers/IsBattleProvider.tsx
+++ app/src/providers/IsBattleProvider.tsx
@@ -1,4 +1,5 @@
 import battle from '@/recolis/battle'
+import BattleType from '@/types/BattleType'
 import axios from 'axios'
 import React, { useEffect } from 'react'
 import { Outlet, useNavigate } from 'react-router-dom'
@@ -9,8 +10,9 @@ const IsBattleProvider = () => {
   const setBattle = useSetRecoilState(battle)
 
   useEffect(() => {
-    axios.get('battle')
+    axios.get<BattleType>('battle')
       .then(e => {
+        if (!e.data.isActive) return
         setBattle(e.data)
         navigate('battle/ready')
       })
